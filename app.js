d61ac87d93dbb4ab561787156dd5b38c3ff1aa5b
Stephen Kirby <me@skirby.dev>
2021-03-02 13:49:30 -0600

    Properly converted to Express.

--- app.js
+++ app.js
@@ -2,23 +2,20 @@
 const http = require('http');
 
 // express
-var express = require('express');
+const express = require('express');
 
 // Import mongodb
-var MongoClient = require('mongodb').MongoClient;
+const MongoClient = require('mongodb').MongoClient;
+
+// Express App
+const app = express();
 
 // Configure Server details
 const hostname = '127.0.0.1';
 const port = 3000;
 
-// Open Server
-const server = http.createServer((req, res) => {
-  res.statusCode = 200;
-  res.setHeader('Content-Type', 'text/plain');
-  res.end('Hello World');
-});
 
-// listen
-server.listen(port, hostname, () => {
-  console.log(`Server running at http://${hostname}:${port}/`);
-});
+// Open express app
+app.listen(port, function() {
+    console.log('Listining on port:' + port)
+})
