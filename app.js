b28cb611dfe68c6e17acba293eecac0d9341de11
Stephen Kirby <me@skirby.dev>
2021-03-02 13:54:24 -0600

    Able to request simple html file.

--- app.js
+++ app.js
@@ -1,21 +1,22 @@
-// Import http module
 const http = require('http');
+const path = require('path');
 
-// express
+
 const express = require('express');
-
-// Import mongodb
 const MongoClient = require('mongodb').MongoClient;
+
 
 // Express App
 const app = express();
-
-// Configure Server details
 const hostname = '127.0.0.1';
 const port = 3000;
 
+// Set up request and response 
+app.get('/', function (req, res) {
+    res.sendFile(path.join(__dirname + '/index.html'));
+})
 
 // Open express app
 app.listen(port, function() {
-    console.log('Listining on port:' + port)
+    console.log('Listining on port:' + port);
 })
